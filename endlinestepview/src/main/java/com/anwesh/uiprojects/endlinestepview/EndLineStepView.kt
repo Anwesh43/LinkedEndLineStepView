b2eae57cb0e844833b33bde548ce0ed0cf57b4bd
Anwesh43 <anweshthecool0@gmail.com>
2018-09-30 02:12:05 +0530

    importing libraries and defining nodes

--- endlinestepview/src/main/java/com/anwesh/uiprojects/endlinestepview/EndLineStepView.kt
+++ endlinestepview/src/main/java/com/anwesh/uiprojects/endlinestepview/EndLineStepView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.endlinestepview
 /**
  * Created by anweshmishra on 30/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
