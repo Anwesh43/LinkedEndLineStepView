244d1f3898f7952695727167441b00423cc6b873
Anwesh43 <anweshthecool0@gmail.com>
2018-09-30 02:49:42 +0530

    adding drawELSNode canvas extension method

--- endlinestepview/src/main/java/com/anwesh/uiprojects/endlinestepview/EndLineStepView.kt
+++ endlinestepview/src/main/java/com/anwesh/uiprojects/endlinestepview/EndLineStepView.kt
@@ -13,6 +13,31 @@ import android.graphics.Color
 
 val nodes : Int = 5
 
+fun Canvas.drawELSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / 3
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.color = Color.parseColor("#00C853")
+    save()
+    translate(w/2, i * gap + gap)
+    for (j in 0..1) {
+        val sf : Float = 1f - 2 * j
+        val sc : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f)) * 2
+        val sc1 : Float = Math.min(0.5f, sc) * 2
+        val sc2 : Float = Math.min(0.5f, Math.max(0f, sc - 0.5f)) * 2
+        save()
+        scale(sf, 1f)
+        translate((w/2 - size) * (1 - sc1), 0f)
+        rotate(90f * sc2)
+        drawLine(0f, 0f, size, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
 class EndLineStepView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
