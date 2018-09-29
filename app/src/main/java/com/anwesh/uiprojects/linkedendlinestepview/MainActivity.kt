73a75c6e6c9bfba38a1dfa2744ff5be6ec1eecd2
Anwesh43 <anweshthecool0@gmail.com>
2018-09-30 03:15:48 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedendlinestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedendlinestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedendlinestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.endlinestepview.EndLineStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        EndLineStepView.create(this)
     }
 }
