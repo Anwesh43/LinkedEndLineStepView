ad205924fbffc9ebd4d7264862771677ee51ce92
Anwesh43 <anweshthecool0@gmail.com>
2018-09-30 02:57:02 +0530

    adding ELSNode class

--- endlinestepview/src/main/java/com/anwesh/uiprojects/endlinestepview/EndLineStepView.kt
+++ endlinestepview/src/main/java/com/anwesh/uiprojects/endlinestepview/EndLineStepView.kt
@@ -102,4 +102,49 @@ class EndLineStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ELSNode(var i : Int, val state : State = State()) {
+
+        private var next : ELSNode? = null
+        private var prev : ELSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = ELSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawELSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+
+        fun getNext(dir : Int, cb : () -> Unit) : ELSNode {
+            var curr : ELSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
